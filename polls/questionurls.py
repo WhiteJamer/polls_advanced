900778d40ee5ccc08b852124412d3f330007ee6c
whitejamer <mihailkuzin.job@yandex.ru>
2020-05-27 18:08:05 +0300

    Add field questions to PollSerializer

--- polls/questionurls.py
+++ polls/questionurls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('questions/create/', views.CreateQuestion.as_view(), name='create_question'),
-    path('questions/<int:pk>/', views.RetrieveQuestion.as_view(), name='get_question'),
-    path('questions/', views.ListQuestion.as_view(), name='list_question'),
-    path('questions/<int:pk>/update', views.UpdateQuestion.as_view(), name='update_question'),
-    path('questions/<int:pk>/destroy', views.DestroyQuestion.as_view(), name='destroy_question'),
+    path('create/', views.CreateQuestion.as_view(), name='create_question'),
+    path('<int:pk>/', views.RetrieveQuestion.as_view(), name='get_question'),
+    path('', views.ListQuestion.as_view(), name='list_question'),
+    path('<int:pk>/update', views.UpdateQuestion.as_view(), name='update_question'),
+    path('<int:pk>/destroy', views.DestroyQuestion.as_view(), name='destroy_question'),
 ]
--- polls/serializers.py
+++ polls/serializers.py
@@ -1,22 +1,6 @@
 from rest_framework import serializers
 from .models import Poll, Question
 
-class PollSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Poll
-        fields = '__all__'
-
-class PollCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Poll
-        fields = '__all__' # доступны все поля
-
-class PollUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Poll
-        exclude = ('start_date',) # доступны для изменения все поля кроме start_date
-
-
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
@@ -31,5 +15,22 @@ class QuestionUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = '__all__'
+
+class PollSerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(many=True)
+    class Meta:
+        model = Poll
+        fields = ('id', 'name', 'start_date', 'end_date',
+                  'description', 'is_active', 'questions')
+
+class PollCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Poll
+        fields = '__all__' # доступны все поля
+
+class PollUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Poll
+        exclude = ('start_date',) # доступны для изменения все поля кроме start_date
 
 
